c8cbd80a9bbf140105e5b9c4186e52e3ca4b8bbb
getoxdev <getoxdev@gmail.com>
2019-12-21 18:57:05 +0530

    Worked on home fragment

--- app/src/main/java/com/github/tenx/tecnoesis20admin/ui/main/home/HomeFragment.java
+++ app/src/main/java/com/github/tenx/tecnoesis20admin/ui/main/home/HomeFragment.java
@@ -14,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 import com.github.tenx.tecnoesis20admin.R;
@@ -28,13 +29,6 @@ public class HomeFragment extends Fragment {
 //    google fragment lifecycle or https://developer.android.com/guide/components/fragments if you are unsure about how to use fragment lifecycle
 
     private HomeViewModel mViewModel;
-    private ArrayList<String> mImageUrls = new ArrayList<>();
-    private ArrayList<String> mTitles = new ArrayList<>();
-    private ArrayList<String> mDates = new ArrayList<>();
-
-
-    @BindView(R.id.frag_home_rv_card)
-    RecyclerView cardRecyclerView;
 
 
     public static HomeFragment newInstance() {
@@ -44,8 +38,12 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View parent = inflater.inflate(R.layout.fragment_home, container, false);
+        View parent =  inflater.inflate(R.layout.fragment_home, container, false);
         ButterKnife.bind(this, parent);
+
+
+
+
         return parent;
     }
 
@@ -56,25 +54,25 @@ public class HomeFragment extends Fragment {
         // TODO: Use the ViewModel this is for demo delete this later
         mViewModel.loadEvents();
 
-
     }
 
     @Override
     public void onStart() {
         super.onStart();
 
-//        @TODO how to call view model demo
 
+//        @TODO how to call view model demo
+//
         mViewModel.getEvents().observe(this, eventsResponse -> {
 //            display result
-            for (EventResponse res : eventsResponse) {
-                mImageUrls.add(res.getImageUrl());
-                mDates.add(res.getDate());
-                mTitles.add(res.getName());
+            String display = "";
+            for (EventResponse res : eventsResponse){
+                display += res.getName() + "\n";
+                display += res.getDate() + "\n";
+                display += res.getImageUrl() + "\n";
+                display += "-------------------------\n";
             }
-            initRecyclerView();
         });
-
     }
 
     @Override
@@ -82,12 +80,4 @@ public class HomeFragment extends Fragment {
         super.onAttach(context);
 
     }
-
-    private void initRecyclerView() {
-        HomeEventAdapter adapter = new HomeEventAdapter(getActivity(), mImageUrls, mTitles, mDates);
-        cardRecyclerView.setAdapter(adapter);
-        cardRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-    }
-
-
 }
